23ca5d7abc44734ed61cbae3d85ce3ea4b653dc6
miily8310s <rillakerika@gmail.com>
2021-05-07 08:34:29 +0900

    change all songs page

--- pages/songs/index.tsx
+++ pages/songs/index.tsx
@@ -1,9 +1,30 @@
-export const SongsPage = (): JSX.Element => {
+import { GetStaticProps } from 'next'
+
+import Layout from '@/components/Layout'
+import SongItem from '@/components/SongItem'
+
+import { API_URL } from '@/config/index'
+
+export const SongsPage = ({ songs }): JSX.Element => {
   return (
-    <div>
-      <h1>Recommend Songs</h1>
-    </div>
+    <Layout>
+      <h1>All Recommend Songs</h1>
+      {songs.length === 0 && <h3>No songs...</h3>}
+      {songs.map((song) => (
+        <SongItem key={song.id} song={song} />
+      ))}
+    </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(`${API_URL}/api/songs`)
+  const songs = await res.json()
+
+  return {
+    props: { songs },
+    revalidate: 1,
+  }
 }
 
 export default SongsPage
