1bb8d499128a90729e87e0cd3f38c633ccb07c5c
miily8310s <rillakerika@gmail.com>
2021-05-09 11:20:13 +0900

    add song add page

--- pages/songs/add.tsx
+++ pages/songs/add.tsx
@@ -1,8 +1,152 @@
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import Layout from '@/components/Layout'
+
+import { API_URL } from '@/config/index'
+// import styles from '@/styles/Add.module.scss'
+
 export const AddSongPage = (): JSX.Element => {
+  const [values, setValues] = useState({
+    name: '',
+    artist: '',
+    label: '',
+    album: '',
+    date: '',
+    description: '',
+    youtube: '',
+    spotify: '',
+  })
+
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const hasEmptyFields = Object.values(values).some(
+      (element) => element === ''
+    )
+
+    if (hasEmptyFields) {
+      toast.error('Please set values in all fields...')
+    }
+
+    const res = await fetch(`${API_URL}/songs`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    })
+
+    if (!res.ok) {
+      toast.error('Something Went Wrong')
+    } else {
+      const song = await res.json()
+      router.push(`/songs/${song.slug}`)
+    }
+  }
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+  }
+
   return (
-    <div>
+    <Layout title="Add New Song">
+      <Link href="/songs">Go Back</Link>
       <h1>Add Song</h1>
-    </div>
+      <ToastContainer />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <label htmlFor="name">Song Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="artist">Artist</label>
+            <input
+              type="text"
+              id="artist"
+              name="artist"
+              value={values.artist}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="label">Label</label>
+            <input
+              type="text"
+              id="label"
+              name="label"
+              value={values.label}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="album">album</label>
+            <input
+              type="text"
+              id="album"
+              name="album"
+              value={values.album}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Release Date</label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={values.date}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Description</label>
+            <textarea
+              // type="text"
+              id="description"
+              name="description"
+              value={values.description}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Youtube Link</label>
+            <input
+              type="text"
+              id="youtube"
+              name="youtube"
+              value={values.youtube}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Spotify Link</label>
+            <input
+              type="text"
+              id="spotify"
+              name="spotify"
+              value={values.spotify}
+              onChange={handleInputChange}
+            />
+          </div>
+          <input type="submit" value="Add Event" className="btn" />
+        </div>
+      </form>
+    </Layout>
   )
 }
 
