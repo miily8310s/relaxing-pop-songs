df6bf7e719aca0c63de579e024d5f0d0300fc87a
miily8310s <rillakerika@gmail.com>
2021-05-09 18:52:17 +0900

    delete backend directory

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a3eb58868252a847effb2b34e038b3db'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '3c72067e-1829-499b-a042-bc8c7b7870f5'
-};
