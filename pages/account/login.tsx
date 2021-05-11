3d7c7850dc16593e03e098cddd6616c2df341290
miily8310s <rillakerika@gmail.com>
2021-05-11 21:37:31 +0900

    add Login page

---
+++ pages/account/login.tsx
@@ -0,0 +1,61 @@
+import { FaUserAlt } from 'react-icons/fa'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import Link from 'next/link'
+
+import Layout from '@/components/Layout'
+
+import styles from '@/styles/AuthForm.module.scss'
+
+export const LoginPage = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log({ email, password })
+  }
+
+  return (
+    <Layout title="User Login">
+      <div className={styles.auth}>
+        <h1>
+          <FaUserAlt /> Login
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.auth__bottom}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+            />
+          </div>
+          <div className={styles.auth__bottom}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+            />
+          </div>
+          <input type="submit" value="Login" className="btn" />
+        </form>
+        <p>
+          Don't you have an account?{' '}
+          <Link href="/account/register">Register</Link>
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default LoginPage
